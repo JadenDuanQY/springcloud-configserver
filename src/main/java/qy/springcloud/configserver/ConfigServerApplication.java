fecef69d4d75858ca0858a75d188ee1aa906791e
jaden <18636621167@163.com>
2018-12-08 11:24:31 +0800

    configserver

---
+++ src/main/java/qy/springcloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package qy.springcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: Jaden
+ * @Description:
+ * @Date: Created in 10:35 2018/12/8
+ * @Modified By:
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
